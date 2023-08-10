a67cb8da1697ce8b8ee1c74779b8ef0f1eb35e48
MD NASRULLAH <mn.limon.239@gmail.com>
2023-08-10 22:10:18 +0600

    hgftr

--- app/src/main/java/com/rubi/englishshikkha/Splash.java
+++ app/src/main/java/com/rubi/englishshikkha/Splash.java
@@ -2,7 +2,10 @@ package com.rubi.englishshikkha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //For Splash Screen
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Code here
+                Intent myIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, 5000);
+        //==================================
     }
 }
