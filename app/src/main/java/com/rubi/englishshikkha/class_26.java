8554011e08cd587cd52c0d928131a8e9c9ba3400
MD NASRULLAH <mn.limon.239@gmail.com>
2023-08-15 12:17:52 +0600

    new activity added

---
+++ app/src/main/java/com/rubi/englishshikkha/class_26.java
@@ -0,0 +1,14 @@
+package com.rubi.englishshikkha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class class_26 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class26);
+    }
+}
